ee1ba8f6dfd8f568f1e1307150a16ed11a31d9b6
Michal Malinowski <meehow99@gmail.com>
2024-12-09 11:03:55 +0100

    clear secret

--- frontend/apps/web/src/FormsApp.tsx
+++ frontend/apps/web/src/FormsApp.tsx
@@ -50,8 +50,8 @@ export const FormsApp = (props: {}) => {
 			fromAPIRawValue: _ => _ == undefined ? List() : List(_),
 			toAPIRawValue: _ => _.valueSeq().toArray()
 		},
-		"base64File": { fromAPIRawValue: _ => typeof _ == "string" ? _ : "", toAPIRawValue: _ => "" },
-		"secret": { fromAPIRawValue: _ => typeof _ == "string" ? _ : "", toAPIRawValue: _ => _ },
+		"base64File": { fromAPIRawValue: _ => typeof _ == "string" ? _ : "", toAPIRawValue: _ => _ },
+		"secret": { fromAPIRawValue: _ => typeof _ == "string" ? _ : "", toAPIRawValue: _ => "" },
 
 	}
 
