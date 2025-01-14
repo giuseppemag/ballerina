2cf7f7212e021eae3801054f76312f3f208d2325
John Fletcher <john.fletcher@blp-digitial.com>
2025-01-14 16:02:44 +0100

    add error monad

--- frontend/libraries/ballerina-core/src/collections/domains/valueOrError/state.ts
+++ frontend/libraries/ballerina-core/src/collections/domains/valueOrError/state.ts
@@ -1,14 +1,65 @@
-export type ValueOrError<v, e> = { value: v } | e[];
+import { BasicFun, Fun } from "../../../fun/state";
+import { Value } from "../../../value/state";
+
+export type ValueOrError<v, e> =
+  | (Value<v> & { kind: "value" })
+  | { errors: e[]; kind: "errors" };
 export const ValueOrError = {
-  Default: <v, e>(v: v): ValueOrError<v, e> => ({ value: v }),
-  Updaters: {
-    value: <v, e>(_: BasicUpdater<v>): Updater<ValueOrError<v, e>> =>
-      Updater<ValueOrError<v, e>>((current) => ({
-        ...current,
-        value: _(current.value),
-      })),
+  Default: {
+    value: <v, e>(_: v): ValueOrError<v, e> => ({
+      ...Value.Default(_),
+      kind: "value",
+    }),
+    errors: <v, e>(_: e[]): ValueOrError<v, e> => ({
+      errors: _,
+      kind: "errors",
+    }),
   },
   Operations: {
-    value: <v, e>(_: ValueOrError<v, e>): v => _.value,
+    return: <v, e>(_: v): ValueOrError<v, e> => ValueOrError.Default.value(_),
+    throw: <v, e>(_: e): ValueOrError<v, e> => ValueOrError.Default.errors([_]),
+    map: <a, b, e>(
+      f: BasicFun<a, b>
+    ): Fun<ValueOrError<a, e>, ValueOrError<b, e>> =>
+      Fun((_) => {
+        if (_.kind == "errors") {
+          return _;
+        }
+        return ValueOrError.Default.value(f(_.value));
+      }),
+    flatten: <a, e>(): Fun<
+      ValueOrError<ValueOrError<a, e>, e>,
+      ValueOrError<a, e>
+    > =>
+      Fun((_) => {
+        if (_.kind == "errors") {
+          return _;
+        } else if (_.value.kind == "errors") {
+          return ValueOrError.Default.errors(_.value.errors);
+        } else {
+          return _.value;
+        }
+      }),
+    then: <a, b, e>(
+      k: BasicFun<a, ValueOrError<b, e>>
+    ): BasicFun<ValueOrError<a, e>, ValueOrError<b, e>> =>
+      Fun((_) =>
+        ValueOrError.Operations.map<a, ValueOrError<b, e>, e>(k).then(
+          ValueOrError.Operations.flatten<b, e>()
+        )(_)
+      ),
+    all: <v, e>(_: ValueOrError<v, e>[]): ValueOrError<v[], e> => {
+      if (_.every((_) => _.kind == "value")) {
+        return ValueOrError.Default.value(_.map((_) => _.value));
+      }
+      return ValueOrError.Default.errors(
+        _.reduce((acc, curr) => {
+          if (curr.kind == "errors") {
+            return [...acc, ...curr.errors];
+          }
+          return acc;
+        }, [] as e[])
+      );
+    },
   },
 };
