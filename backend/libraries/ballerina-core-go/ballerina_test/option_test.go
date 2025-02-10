3a9d32392cb751fb6013442f1e1e4d5bac7cc3a9
Davide Walder <davide.walder@gmail.com>
2025-02-10 17:51:33 +0100

    test: test status quo

---
+++ backend/libraries/ballerina-core-go/ballerina_test/option_test.go
@@ -0,0 +1,61 @@
+package ballerina_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	ballerina "Ballerina.com/core"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSomeShouldSerializeCorrectly(t *testing.T) {
+	t.Parallel()
+
+	some := ballerina.Some(4)
+
+	data, err := json.Marshal(some)
+	require.NoError(t, err)
+
+	require.Equal(t, "{\"Value\":4,\"IsSome\":true}", string(data))
+}
+
+func TestNoneShouldSerializeCorrectly(t *testing.T) {
+	t.Parallel()
+
+	some := ballerina.None[int]()
+
+	data, err := json.Marshal(some)
+	require.NoError(t, err)
+
+	require.Equal(t, "{\"Value\":0,\"IsSome\":false}", string(data))
+}
+
+func TestSomeShouldSerializeAndDeserializeCorrectly(t *testing.T) {
+	t.Parallel()
+
+	some := ballerina.Some(4)
+
+	data, err := json.Marshal(some)
+	require.NoError(t, err)
+
+	var target ballerina.Option[int]
+
+	require.NoError(t, json.Unmarshal(data, &target))
+
+	require.Equal(t, some, target)
+}
+
+func TestNoneShouldSerializeAndDeserializeCorrectly(t *testing.T) {
+	t.Parallel()
+
+	some := ballerina.None[int]()
+
+	data, err := json.Marshal(some)
+	require.NoError(t, err)
+
+	var target ballerina.Option[int]
+
+	require.NoError(t, json.Unmarshal(data, &target))
+
+	require.Equal(t, some, target)
+}
