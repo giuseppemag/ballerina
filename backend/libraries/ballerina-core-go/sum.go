7cda023293eeefd0a16de27a5f5c96aa385b72bc
Lauren Friedmann <lauren.friedmann@blp-digital.com>
2025-03-19 18:20:42 +0100

    update Sum generic (#114)
    * update Sum generic
    
    * add more elegant serialization

--- backend/libraries/ballerina-core-go/sum.go
+++ backend/libraries/ballerina-core-go/sum.go
@@ -1,6 +1,7 @@
 package ballerina
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
@@ -8,6 +9,27 @@ type Sum[a any, b any] interface{}
 type left[a any, b any] struct {
 	Sum[a, b]
 	value a
+}
+
+type leftForSerialization[a any, b any] struct {
+	Sum[a, b]
+	Value a `json:"Value,omitempty"`
+}
+
+func (l left[a, b]) MarshalJSON() ([]byte, error) {
+	return json.Marshal(leftForSerialization[a, b]{
+		Value: l.value,
+	})
+}
+
+func (l *left[a, b]) UnmarshalJSON(data []byte) error {
+	target := leftForSerialization[a, b]{}
+	if err := json.Unmarshal(data, &target); err != nil {
+		return err
+	}
+	l.value = target.Value
+	l.Sum = target.Sum
+	return nil
 }
 
 func Left[a any, b any](value a) Sum[a, b] {
@@ -21,11 +43,33 @@ type right[a any, b any] struct {
 	value b
 }
 
+type rightForSerialization[a any, b any] struct {
+	Sum[a, b]
+	Value b `json:"Value,omitempty"`
+}
+
+func (l right[a, b]) MarshalJSON() ([]byte, error) {
+	return json.Marshal(rightForSerialization[a, b]{
+		Value: l.value,
+	})
+}
+
+func (l *right[a, b]) UnmarshalJSON(data []byte) error {
+	target := rightForSerialization[a, b]{}
+	if err := json.Unmarshal(data, &target); err != nil {
+		return err
+	}
+	l.value = target.Value
+	l.Sum = target.Sum
+	return nil
+}
+
 func Right[a any, b any](value b) Sum[a, b] {
 	p := new(right[a, b])
 	p.value = value
 	return p
 }
+
 func MatchSum[a any, b any, c any](self Sum[a, b], onLeft func(a) c, onRight func(b) c) (c, error) {
 	switch v := self.(type) {
 	case left[a, b]:
