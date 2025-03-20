93ab574f5565487a9ca4942a830fb85835cd9b10
Davide Walder <58290976+DavideWalder@users.noreply.github.com>
2025-03-20 08:48:11 +0100

    feat: implement optional with Sum (#118)

--- backend/libraries/ballerina-core-go/option.go
+++ backend/libraries/ballerina-core-go/option.go
@@ -1,39 +1,25 @@
 package ballerina
 
 type Option[a any] struct {
-	Value  a
-	IsSome bool
+	Sum[a, Unit] // NOTE: struct embedding is needed to be able to access Sum's JSON methods
 }
 
 // type some[a any] struct { Option[a]; Value a; Kind string }
 func Some[a any](value a) Option[a] {
-	p := Option[a]{
-		Value:  value,
-		IsSome: true,
-	}
-	return p
+	return Option[a]{Left[a, Unit](value)}
 }
 
 // type none[a any] struct { Option[a]; Kind string }
 func None[a any]() Option[a] {
-	p := Option[a]{
-		IsSome: false,
-	}
-	return p
+	return Option[a]{Right[a](DefaultUnit)}
 }
+
 func MatchOption[a any, c any](self Option[a], onSome func(a) c, onNone func() c) c {
-	if self.IsSome {
-		return onSome(self.Value)
-	} else {
-		return onNone()
-	}
+	return Fold(self.Sum, onSome, func(_ Unit) c { return onNone() })
 }
+
 func MapOption[a any, b any](self Option[a], f func(a) b) Option[b] {
-	if self.IsSome {
-		return Some(f(self.Value))
-	} else {
-		return None[b]()
-	}
+	return Option[b]{BiMap(self.Sum, f, id)}
 }
 
 type WriterOption[Delta any, DeltaA any] interface {
