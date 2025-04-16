48f9dc23d1aade3f9f19493b3006a9e3e71c9187
David Marx <63108051+david-marx@users.noreply.github.com>
2025-04-16 13:53:53 +0200

    chore: extend odered map updater with concat and upsert (#150)

--- frontend/libraries/ballerina-core/src/fun/domains/updater/domains/orderedMapUpdater/state.ts
+++ frontend/libraries/ballerina-core/src/fun/domains/updater/domains/orderedMapUpdater/state.ts
@@ -1,4 +1,5 @@
-import { Fun } from "../../../../state";
+import { BasicFun, Fun } from "../../../../state";
+import { unit, Unit } from "../../../unit/state";
 import { BasicUpdater, Updater } from "../../state";
 import { OrderedMap } from "immutable";
 
@@ -13,7 +14,12 @@ export type OrderedMapUpdater<
       [f in DisplayName]: {
         add: Fun<[key, value], Updater<Entity>>;
         remove: Fun<key, Updater<Entity>>;
+        concat: Fun<OrderedMap<key, value>, Updater<Entity>>;
         set: Fun<key, Fun<BasicUpdater<value>, Updater<Entity>>>;
+        upsert: Fun<
+          [key, BasicFun<Unit, value>, BasicUpdater<value>],
+          Updater<Entity>
+        >;
       };
     }
   : "Error: orderedMapUpdater has been invoked on a field which is not an OrderedMap";
@@ -36,7 +42,7 @@ export const orderedMapUpdater =
                     key,
                     value,
                   ),
-                }) as Entity,
+                } as Entity),
             ),
         ),
         remove: Fun(
@@ -48,7 +54,19 @@ export const orderedMapUpdater =
                   [field]: (
                     entity[field] as OrderedMap<unknown, unknown>
                   ).remove(key),
-                }) as Entity,
+                } as Entity),
+            ),
+        ),
+        concat: Fun(
+          (other: OrderedMap<unknown, unknown>): Updater<Entity> =>
+            Updater(
+              (entity) =>
+                ({
+                  ...entity,
+                  [field]: (
+                    entity[field] as OrderedMap<unknown, unknown>
+                  ).concat(other),
+                } as Entity),
             ),
         ),
         set: Fun((key: unknown) =>
@@ -71,9 +89,32 @@ export const orderedMapUpdater =
                               ).get(key)!,
                             ),
                           ),
-                  }) as Entity,
+                  } as Entity),
               ),
           ),
         ),
+        upsert: Fun(
+          ([key, fallback, fieldUpdater]: [
+            unknown,
+            BasicFun<Unit, unknown>,
+            BasicUpdater<unknown>,
+          ]): Updater<Entity> =>
+            Updater(
+              (entity) =>
+                ({
+                  ...entity,
+                  [field]: (entity[field] as OrderedMap<unknown, unknown>).set(
+                    key,
+                    (entity[field] as OrderedMap<unknown, unknown>).has(key)
+                      ? fieldUpdater(
+                          (entity[field] as OrderedMap<unknown, unknown>).get(
+                            key,
+                          )!,
+                        )
+                      : fieldUpdater(fallback(unit)),
+                  ),
+                } as Entity),
+            ),
+        ),
       },
-    }) as OrderedMapUpdater<Entity, Field, DisplayName>;
+    } as OrderedMapUpdater<Entity, Field, DisplayName>);
