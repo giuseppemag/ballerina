5eca12e750a0af354ac2e324c5e828af33fbac64
Giuseppe Maggiore <giuseppemag@gmail.com>
2025-04-16 06:45:39 +0000

    improve error messages when missing parts of table form renderer

--- backend/libraries/ballerina-core/business rule engine/form engine/parser.fs
+++ backend/libraries/ballerina-core/business rule engine/form engine/parser.fs
@@ -837,21 +837,24 @@ module Parser =
                   state {
                     let! form = formsState.TryFindForm s |> state.OfSum
 
-                    match form.Body with
-                    | FormBody.Union cases ->
-                      let formType = cases.UnionType
-                      return FormRenderer(form |> FormConfig.Id, formType, children)
-                    | FormBody.Record fields -> return FormRenderer(form |> FormConfig.Id, fields.RecordType, children)
-                    | FormBody.Table table ->
-                      // do Console.WriteLine form.FormName
-                      // do Console.WriteLine parentJsonFields.ToFSharpString
-                      // do Console.ReadLine() |> ignore
-                      let! tableApiNameJson = parentJsonFields |> sum.TryFindField "api" |> state.OfSum
-                      let! tableApiName = tableApiNameJson |> JsonValue.AsString |> state.OfSum
-                      let! (tableApi) = formsState.TryFindTableApi tableApiName |> state.OfSum
-                      let! tableType = formsState.TryFindType tableApi.TypeId.TypeName |> state.OfSum
+                    return! state{
+                      match form.Body with
+                      | FormBody.Union cases ->
+                        let formType = cases.UnionType
+                        return FormRenderer(form |> FormConfig.Id, formType, children)
+                      | FormBody.Record fields -> return FormRenderer(form |> FormConfig.Id, fields.RecordType, children)
+                      | FormBody.Table table ->
+                        // do Console.WriteLine form.FormName
+                        // do Console.WriteLine parentJsonFields.ToFSharpString
+                        // do Console.ReadLine() |> ignore
+                        let! tableApiNameJson = parentJsonFields |> sum.TryFindField "api" |> state.OfSum
+                        let! tableApiName = tableApiNameJson |> JsonValue.AsString |> state.OfSum
+                        let! (tableApi) = formsState.TryFindTableApi tableApiName |> state.OfSum
+                        let! tableType = formsState.TryFindType tableApi.TypeId.TypeName |> state.OfSum
 
-                      return FormRenderer(form |> FormConfig.Id, tableType.Type |> ExprType.TableType, children)
+                        return FormRenderer(form |> FormConfig.Id, tableType.Type |> ExprType.TableType, children)
+                    } |> state.MapError(Errors.WithPriority ErrorPriority.High)
+
                   }
                   state.Throw(
                     Errors.Singleton
